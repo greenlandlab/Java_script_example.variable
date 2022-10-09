ec0b4d293fcd22a5b011d2265fdc7a47fdb26ada
Ayaan <112767901+greenlandlab@users.noreply.github.com>
2022-10-09 10:55:05 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        int lesson_var1 = 12
+         // int or intiger is a datatype that can only contain 1 peach of number not 1 digit number only 1 number for example 12
+         String lesson_var2 = "Hello how are you? I am waiting for something for 2 hours tho they said hi sorry for being late it's coming today its true not false c u 1.9"
+         // a String is a datatype that can have all the datatypes as it's value for example the example var has all the datatypes buts it's a string thats why it's ok for the syntax
+         char lesson_var3 = "o"
+         // a char datatype can only have a one dight number or only one letter
+         boolean lesson_var4 = true
+         boolean lesson_var5 = false
+         float lesson_var6 = 1.9
+         // the float datatype has to be a number with a point not only 1 or any number but it could still be any_number.00
+          
+          
+          
+}
